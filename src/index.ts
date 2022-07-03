b89ce093e218d28882b0d6fc1c996f26566db675
Samuel Gratzl <sam@sgratzl.com>
2022-07-03 14:12:20 -0400

    feat: re export projections

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,21 @@ export * from './scales';
 export * from './elements';
 export * from './controllers';
 export * as topojson from 'topojson-client';
+
+export {
+  geoAzimuthalEqualArea,
+  geoAzimuthalEquidistant,
+  geoGnomonic,
+  geoOrthographic,
+  geoStereographic,
+  geoEqualEarth,
+  geoAlbers,
+  geoAlbersUsa,
+  geoConicConformal,
+  geoConicEqualArea,
+  geoConicEquidistant,
+  geoEquirectangular,
+  geoMercator,
+  geoTransverseMercator,
+  geoNaturalEarth1,
+} from 'd3-geo';

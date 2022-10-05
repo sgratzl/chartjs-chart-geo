961f5eaa4fd45951060f175227577fe28a67bfea
Samuel Gratzl <sam@sgratzl.com>
2022-10-04 22:46:51 -0400

    build: use different mapper for jest

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 /* eslint-env node */
-const path = require('path');
+// const path = require('path');
 
 module.exports = {
   testEnvironment: 'jsdom',
   preset: 'ts-jest/presets/js-with-ts-esm',
   moduleNameMapper: {
-    '^d3-(.*)$': `d3-$1${path.sep}dist${path.sep}d3-$1`,
+    '^d3-(.*)$': `d3-$1\\dist\\d3-$1`,
   },
   rootDir: './src',
   testRegex: '((\\.|/)(test|spec))\\.m?tsx?$',

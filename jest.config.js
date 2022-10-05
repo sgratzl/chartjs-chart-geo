76dc39814895a5e14b04a65dba776350b8f8fb36
Samuel Gratzl <sam@sgratzl.com>
2022-10-04 22:31:44 -0400

    build: fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
 /* eslint-env node */
+const path = require('path');
 
 module.exports = {
   testEnvironment: 'jsdom',
   preset: 'ts-jest/presets/js-with-ts-esm',
+  moduleNameMapper: {
+    '^d3-(.*)$': `d3-$1${path.sep}dist${path.sep}d3-$1`,
+  },
   rootDir: './src',
-  testRegex: '((\\.|/)(test|spec))\\.tsx?$',
+  testRegex: '((\\.|/)(test|spec))\\.m?tsx?$',
 };

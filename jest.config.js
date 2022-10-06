ae786dc3464e102dd31bdcdea0ad5c2b1572d606
Samuel Gratzl <sam@sgratzl.com>
2022-10-05 21:54:57 -0400

    build: fix try using min version

--- jest.config.js
+++ jest.config.js
@@ -4,9 +4,8 @@ const path = require('path');
 module.exports = {
   testEnvironment: 'jsdom',
   preset: 'ts-jest/presets/js-with-ts-esm',
-  // transformIgnorePatterns: ['node_modules/(?!(d3-.*)/)'],
   moduleNameMapper: {
-    '^d3-(.*)$': [`d3-$1${path.sep}dist${path.sep}d3-$1`, '<rootDir>/node_modules/d3-$1/dist/d3-$1.js'],
+    '^d3-(.*)$': `d3-$1${path.sep}dist${path.sep}d3-$1.min`,
   },
   testRegex: '((\\.|/)(test|spec))\\.m?tsx?$',
 };

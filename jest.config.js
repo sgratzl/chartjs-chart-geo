6cb66deecf209abc6c6ce6d68be3f07fca353801
Samuel Gratzl <sam@sgratzl.com>
2022-10-05 22:20:26 -0400

    build: try to explicitly list mapper

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,14 @@
 /* eslint-env node */
 const path = require('path');
 
+const mapper = {};
+for (const d of ['d3-array', 'd3-geo', 'd3-scale-chromatic', 'd3-interpolate', 'd3-color']) {
+  mapper[`^${d}$`] = require.resolve(d).replace(`src${path.sep}index.js`, `dist${path.sep}/${d}.js`);
+}
+
 module.exports = {
   testEnvironment: 'jsdom',
   preset: 'ts-jest/presets/js-with-ts-esm',
-  moduleNameMapper: {
-    '^d3-(.*)$': `d3-$1${path.sep}dist${path.sep}d3-$1.min`,
-  },
+  moduleNameMapper: mapper,
   testRegex: '((\\.|/)(test|spec))\\.m?tsx?$',
 };

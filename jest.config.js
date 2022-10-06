00898c4366ba12cb48c795e7e9acf828d0049b04
Samuel Gratzl <sam@sgratzl.com>
2022-10-05 21:39:50 -0400

    ci: more outputs

--- jest.config.js
+++ jest.config.js
@@ -4,9 +4,9 @@ const path = require('path');
 module.exports = {
   testEnvironment: 'jsdom',
   preset: 'ts-jest/presets/js-with-ts-esm',
+  // transformIgnorePatterns: ['node_modules/(?!(d3-.*)/)'],
   moduleNameMapper: {
-    '^d3-(.*)$': [`d3-$1${path.sep}dist${path.sep}d3-$1`, '<rootDir>/../node_modules/d3-$1/dist/d3-$1.js'],
+    '^d3-(.*)$': [`d3-$1${path.sep}dist${path.sep}d3-$1`, '<rootDir>/node_modules/d3-$1/dist/d3-$1.js'],
   },
-  rootDir: './src',
   testRegex: '((\\.|/)(test|spec))\\.m?tsx?$',
 };
